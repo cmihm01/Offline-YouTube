3e31d2751ae5abc03f863d0edb5bda661253c393
Mateo Davis <mateo.cohen.davis@gmail.com>
2017-04-10 21:45:50 -0400

    combined app and teacher app

--- teacher_int/app.py
+++ teacher_int/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
 import httplib2
 import os
 import re
 import sys
+import pafy, zipfile, math
 
 
 from apiclient.discovery import build
@@ -49,7 +50,48 @@ def youtube_search(cat,diff,age,keyw):
 		                             search_result["id"]["videoId"]))
 
 	print "Videos:\n", "\n".join(videos), "\n"
-	return render_template("result.html",videos=videos)
+	return download_vids(videos) 
+	# need to be URLS
+
+def download_vids(vidArray):
+    max_viewcount = 0
+    scored_vids = []
+# https://stackoverflow.com/questions/273192/how-to-check-if-a-directory-exists-and-create-it-if-necessary
+    if not os.path.exists("static/vids/"):
+        os.makedirs("static/vids/")
+    for x in vidArray:
+        url = "https://www.youtube.com/watch?v=" + x
+        video = pafy.new(url)
+        ##owen's shit
+        score = 0
+        if (video.viewcount > max_viewcount):
+            max_viewcount = video.viewcount
+        score = score + (math.sqrt(video.rating)) + (video.viewcount / max_viewcount) ##+ count
+
+        s = video.allstreams[1]
+        scored_vids.append(tuple((score, s)))
+    scored_vids.sort(key = lambda tup: tup[0], reverse=True)
+    for i in range(0, len(scored_vids)):
+        global count
+        count += 1
+        filename = scored_vids[i][1].download("static/vids/" + str(count) + ".mp4")
+
+        # global count
+        # count += 1
+        # filename = s.download("static/vids/" + str(count) + ".mp4")    
+    return redirect(url_for('done'))
+
+@app.route('/done')
+def done():
+    zipdir('static/vids')
+    return app.send_static_file('content.zip')
+
+# https://stackoverflow.com/questions/1855095/how-to-create-a-zip-archive-of-a-directory/
+def zipdir(path):
+    zf = zipfile.ZipFile("static/content.zip", "w")
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zf.write(os.path.join(root, file))
 
 
 
